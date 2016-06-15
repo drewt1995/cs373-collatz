b121c71b7775f93a3a3baed9291bd966b6953d11
Andrew Turner <drewt1995@gmail.com>
2016-06-15 10:41:05 -0500

    Added optimizations and updates so would work in Travis CI

--- Collatz.py
+++ Collatz.py
@@ -6,16 +6,21 @@
 # Glenn P. Downing
 # ---------------------------
 
+CACHE = {}
+
 # ------------
 # collatz_read
 # ------------
 MINVAL = 0
+TESTVAL = 837799
 MAXVAL = 1000000
+MAXCYCLE = 525
+
 
 def collatz_read(line):
     """
     read two ints
-    s a string
+    line a string
     return a list of two ints, representing the beginning and end of a range, [i, j]
     """
     tokens = line.split()
@@ -32,24 +37,50 @@ def collatz_eval(i, j):
     j the end       of the range, inclusive
     return the max cycle length of the range [i, j]
     """
-    assert isinstance(i, int)
-    assert isinstance(j, int)
+    assert i > 0
+    assert j < 1000000
+    global CACHE
 
-    max_length = 0
-    if i > MINVAL and j < MAXVAL:
-        for value in range(i, j+1):
+    if i <= TESTVAL <= j:
+        return MAXCYCLE
+
+    if not CACHE:
+        CACHE = {1:1}
+        for val in range(2, MAXVAL + 1):
+            temp = val
             count = 1
-            while value != 1:
-                if value % 2 == 0:
-                    value /= 2
+            while val != 1:
+                if val in CACHE:
+                    count += CACHE.get(val) - 1
+                    break
+                if val % 2 == 0:
+                    val >>= 1
+                    count += 1
                 else:
-                    value = (value * 3) + 1
-                count += 1
-            if count > max_length:
-                max_length = count
+                    val = val + (val >> 1) + 1
+                    count += 2
+            CACHE.update({temp:count})
 
-    assert max_length > 0
-    return max_length
+
+    if i > j:
+        temp = j
+        j = i
+        i = temp
+
+    if (j // 2) + 1 > i:
+        values = range((j // 2) + 1, j+1)
+    else:
+        values = range(i, j+1)
+
+
+    max_value = 1
+    for value in values:
+        tmp = CACHE.get(value)
+        if tmp > max_value:
+            max_value = tmp
+
+    assert max_value != 0
+    return max_value
 
 # -------------
 # collatz_print
@@ -59,10 +90,10 @@ def collatz_eval(i, j):
 def collatz_print(writer, start, end, max_length):
     """
     print three ints
-    w a writer
-    i the beginning of the range, inclusive
-    j the end       of the range, inclusive
-    v the max cycle length
+    writer a writer
+    start the beginning of the range, inclusive
+    end the end       of the range, inclusive
+    max_length the max cycle length
     """
     writer.write(str(start) + " " + str(end) + " " + str(max_length) + "\n")
 
@@ -73,8 +104,8 @@ def collatz_print(writer, start, end, max_length):
 
 def collatz_solve(reader, writer):
     """
-    r a reader
-    w a writer
+    reader a reader
+    writer a writer
     """
     for line in reader:
         start, end = collatz_read(line)
